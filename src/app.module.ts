0fa83af0ae24fd7c9b0b97855a1eeb7bb9e2995e
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2023-09-16 16:20:07 +0200

    TypeORM installation and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseInfo } from './db';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: databaseInfo.host,
+      port: databaseInfo.port,
+      username: databaseInfo.username,
+      password: databaseInfo.password,
+      database: 'gym_routine_organizer',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db.ts
@@ -0,0 +1,7 @@
+export const databaseInfo = {
+  host: 'your host',
+  port: 1234,
+  username: 'your username',
+  password: 'your password',
+  database: 'your database',
+};
